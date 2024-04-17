b00f2ce7e59ebced62a8bfa1c6a93038c55b18bf
pr0ctor <20609138+pr0ctor@users.noreply.github.com>
2024-04-17 04:24:00 -0400

    Refactored main Program logic and added a small amount of progress logging Added launchSettings to the gitignore

--- PoeSmdConverter/Program.cs
+++ PoeSmdConverter/Program.cs
@@ -11,46 +11,60 @@ IConfiguration config = new ConfigurationBuilder()
 
 Helpers.ValidateCommandlineArgs(config, args);
 
+var smdPath = config["smd"].Trim();
+var objPath = config["obj"].Trim();
+
 try
 {
-    using var sourceFile = new FileStream(config["smd"].Trim(), FileMode.Open, FileAccess.Read);
+
+    using var sourceFile = new FileStream(smdPath, FileMode.Open, FileAccess.Read);
+    using var destinationFile = new FileStream(objPath, FileMode.OpenOrCreate, FileAccess.Write);
+
+    var smdReader = new SmdReader();
+
+    Console.WriteLine($"Read file from \"{smdPath}\"");
 
     var smdFile = new Smd(sourceFile);
+    var parsedSmd = new Smd();
 
+    // Check file for Brotli Compression
     var potentialBrotliCheckbytes = smdFile.GetBrotliChecksum();
 
+    // Check for the Brotli check bytes from the first few bytes from the source file
     if(Helpers.CheckForBrotliCompression(potentialBrotliCheckbytes))
     {
+        // If the file has Brotli Compression, read the check bytes in order to properly decompress 
         var rawLengthBuffer = new byte[MagicNumbers.TotalFileLengthIndicatorLength];
 
         var rawLength = sourceFile.Read(rawLengthBuffer, 0, MagicNumbers.TotalFileLengthIndicatorLength);
 
         var readableLength = BitConverter.ToInt32(rawLengthBuffer, 0);
 
-        var smdReader = new SmdReader();
-        var parsedSMD = smdReader.ReadBrotliSmdFile(smdFile);
+        Console.WriteLine("\tParsing data from Brotli compressed smd file.");
 
-        using var destinationFile = new FileStream(config["obj"].Trim(), FileMode.OpenOrCreate, FileAccess.Write);
-        var objWriter = new ObjWriter(destinationFile);
-        
-        objWriter.WriteObjFromSmd(parsedSMD);
+        parsedSmd = smdReader.ReadBrotliSmdFile(smdFile);
     }
     else
     {
-        var smdReader = new SmdReader();
+        // If not Brotli Compressed, reset the stream to the start since
+        //      checking the first few bytes causes the pointer to move
         smdFile.ResetFileDataPointer();
-        var parsedSMD = smdReader.ReadSmdFile(smdFile);
 
-        using var destinationFile = new FileStream(config["obj"].Trim(), FileMode.OpenOrCreate, FileAccess.Write);
-        var objWriter = new ObjWriter(destinationFile);
+        Console.WriteLine("\tParsing data from smd file");
 
-        objWriter.WriteObjFromSmd(parsedSMD);
+        parsedSmd = smdReader.ReadSmdFile(smdFile);
     }
+
+    var objWriter = new ObjWriter(destinationFile);
+
+    Console.WriteLine($"Writing processed data to \"{objPath}\"");
+
+    objWriter.WriteObjFromSmd(parsedSmd);
 
 }
 catch(FileNotFoundException ex)
 {
-    Console.WriteLine("Smd file not found at the given location. " + ex.ToString());
+    Console.WriteLine($"Smd file not found at the given location: \"{smdPath}\". " + ex.ToString());
     throw;
 }
 catch(Exception ex)
